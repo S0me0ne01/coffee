ed56c91478704f74454dee0c26d4a09489b5f84e
S0me0ne01 <morethanicloud@icloud.com>
2023-11-15 11:57:36 +0600

    The most basic version

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-layout view="lHh Lpr lFf" style="background-color: #ffffff">
+    <!-- HEADER -->
+    <q-header elevated>
+      <q-toolbar style="background-color: #ffbc7d">
+        <q-btn flat dense round icon="menu" aria-label="Menu" style="color: #dc4e04" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          &nbsp;&nbsp;<a href="/"><img src="/images/logo.png" height="50" style="padding-top: 8px" /></a>
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-not-above bordered
+      style="background: linear-gradient(to bottom, #ffbc7d, #f1b781)">
+      <q-list>
+        <q-item-label header style="color: #dc4e04">
+          Полезные ссылки
+        </q-item-label>
+
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page style="background-color: #FFFFFF">
+    <!-- CAROUSEL -->
+    <div class="q-md">
+      <q-carousel animated v-model="slide" class="carousel" :class="{ 'full-height': isFullHeight }" arrows navigation
+        infinite :autoplay="autoplay" @mouseenter="autoplay = false" @mouseleave="autoplay = true">
+
+        <q-carousel-slide :name="1" img-src="/images/slide1.jpg" />
+        <q-carousel-slide :name="2" img-src="/images/slide2.jpeg" />
+        <q-carousel-slide :name="3" img-src="/images/slide3.jpg" />
+      </q-carousel>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { useQuasar } from 'quasar'
+
+export default defineComponent({
+  name: 'Index',
+
+  setup() {
+    const $q = useQuasar()
+
+    return {
+      slide: ref(1),
+      autoplay: ref(true),
+      isFullHeight: ref(),
+    }
+  }
+})
+</script>
